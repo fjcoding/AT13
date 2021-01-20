d12405e96d303d06f622fcb3052dd56e84d06f7e
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-01-20 18:36:52 -0400

    Add class JuanGonzales.java

---
+++ src/main/java/org/fundacionjala/at13/students/JuanGonzales.java
@@ -0,0 +1,14 @@
+package org.fundacionjala.at13.students;
+public class JuanGonzales{
+/**
+ * -Juan Pablo Gonzalgues Alvarado
+-I like to be called juampi
+-29 years old
+-Ing. Informatico
+-Juan Misael Saracho University
+-preferred programming languages: Java y Dart
+-Tarija
+
+ */
+
+}
