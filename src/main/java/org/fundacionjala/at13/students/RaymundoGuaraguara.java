f737f0900582bb7eaaad7bbdc3302312bf9e57e2
Raymundo Guaraguara <raymundogs@gmail.com>
2021-01-20 19:27:00 -0400

    turn student into java class

--- src/main/java/org/fundacionjala/at13/students/RaymundoGuaraguara.java
+++ src/main/java/org/fundacionjala/at13/students/RaymundoGuaraguara.java
@@ -1,7 +1,14 @@
-Raymundo Guaraguara Sansusty
-I like to be called Ray
-31
-Industrial and systems engineering
-Universidad Privada Boliviana
-Python
-Cochabamba
+package org.fundacionjala.at13.students;
+
+class RaymundoGuaraguara {
+
+    /*
+    Raymundo Guaraguara Sansusty
+    I like to be called Ray
+    31
+    Industrial and systems engineering
+    Universidad Privada Boliviana
+    Python
+    Cochabamba
+    */
+}
