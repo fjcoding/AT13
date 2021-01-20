6f7b413e3bbb983610b3cbde892d60d74791cb8c
SaulCM <saulcaspamiranda@gmail.com>
2021-01-20 19:18:22 -0400

    change file name SaulCaspa.txt

---
+++ src/main/java/org/fundacionjala/at13/students/SaulCaspa.java
@@ -0,0 +1,9 @@
+public class SaulCaspa{
+    /*Saul Caspa Miranda
+    I like to be called just Saul
+    26
+    Systems engineering
+    Universidad Mayor de San Simon
+    Java, php, python
+    Cochabamba*/
+}
