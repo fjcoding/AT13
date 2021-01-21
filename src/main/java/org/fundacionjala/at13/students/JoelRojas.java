54fc1a5b7bde98db6300e0b3726eca9bf4f88002
Joel Rojas <jr.bestone17@gmail.com>
2021-01-21 15:45:17 -0400

    add atributes and constructor to JoelRojas.java

--- src/main/java/org/fundacionjala/at13/students/JoelRojas.java
+++ src/main/java/org/fundacionjala/at13/students/JoelRojas.java
@@ -2,11 +2,21 @@ package org.fundacionjala.at13.students;
 
 public class JoelRojas{
 
-/* Joel Rodrigo Rojas Roman
-Joel
-27
-Systems Engineering
-UMSS
-Java
-Quillacollo,Cochabamba */
+    private String name;
+    private String nickName;
+    private int age;
+    private String carrer;
+    private String university;
+    private String languageList:
+    private String city;
+
+    public JoelRojas(){
+        this.name="Joel Rodrigo Rojas Roman";
+        this.nickName="Joel";
+        this.age=27;
+        this.carrer="Systems Engineering";
+        this.university="UMSS";
+        this.languageList="Java";
+        this.city="Quillacollo, Cochabamba";
+    }
 }
