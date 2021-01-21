a53ac7a17e68d9f1a7450d3286fbb00b8e5eca8b
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-01-21 16:00:46 -0400

    showStudentsInformation

--- src/main/java/org/fundacionjala/at13/students/JuanGonzales.java
+++ src/main/java/org/fundacionjala/at13/students/JuanGonzales.java
@@ -11,4 +11,23 @@ public class JuanGonzales{
 
  */
 
+String name = "Juan Pablo Gonzales Alvarado";
+String nickName = "Juampi";
+int age = 29;
+String career = "Computer Engineer";
+String university = "Juan Misael Saracho";
+String programingLanguage = "Java and Dart";
+String city = "Tarija";
+
+
+public String showStudentsInformation() {
+    return "Hello "+"my name is " + name + "\n" +
+            "I like to be called '" + nickName + "\n" +
+            "I am "+ age +" years old" + "\n" +
+            "I am " + career + "\n" +
+            "I study at " + university+ " university \n" +
+            "My programming languages are " + programingLanguage +"\n" +
+            "My residence city " + city;
+}
+
 }
