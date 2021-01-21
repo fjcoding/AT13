8f208f989cedccd11ce23935db64d664924e30cc
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-01-21 16:54:40 -0400

    Added atributes to DanielaSantaCruz.java

--- src/main/java/org/fundacionjala/at13/students/DanielaSantaCruz.java
+++ src/main/java/org/fundacionjala/at13/students/DanielaSantaCruz.java
@@ -1,24 +1,14 @@
 package org.fundacionjala.at13.students;
 public class DanielaSantaCruz{
 
-    private String name;
-    private String nickname;
-    private int age;
-    private String career;
-    private String university;
-    private String programmingLanguages;
-    private String recidenceCity;
+    private String name = "Daniela Santa Cruz Andrade"; 
+    private String nickname = "Dani";
+    private int age = 22;
+    private String career = "Systems engineering";
+    private String university = "UMSS";
+    private String programmingLanguages = "java";
+    private String recidenceCity = "Cochabamba";
     
-    public DanielaSantaCruz(){
-        this.name = "Daniela Santa Cruz Andrade"; 
-        this.nickname = "Dani";
-        this.age = 22;
-        this.career = "Systems engineering";
-        this.university = "UMSS";
-        this.programmingLanguages = "java";
-        this.recidenceCity = "Cochabamba";
-    }
-
     public String getResidenceCity(){
         return "Currently I live in "+residenceCity+".";
     }
