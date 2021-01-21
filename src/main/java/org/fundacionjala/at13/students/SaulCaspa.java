41f54b7358283a3f1e86161ee96f875b947b3fb6
SaulCM <saulcaspamiranda@gmail.com>
2021-01-21 16:08:30 -0400

    constructor and variables for saulcaspa class

--- src/main/java/org/fundacionjala/at13/students/SaulCaspa.java
+++ src/main/java/org/fundacionjala/at13/students/SaulCaspa.java
@@ -1,5 +1,6 @@
 package org.fundacionjala.at13.students;
 public class SaulCaspa{
+
     /*Saul Caspa Miranda
     I like to be called just Saul
     26
@@ -7,4 +8,24 @@ public class SaulCaspa{
     Universidad Mayor de San Simon
     Java, php, python
     Cochabamba*/
+
+    private String name;
+    private String nickName;
+    private int age;
+    private String career;
+    private String university;
+    private String programmingLanguages;
+    private String residenceCity;
+
+    public SaulCaspa(){
+        this.name="Saul Caspa Miranda";
+        this.preferredName="Saul";
+        this.age=26;
+        this.career="Systems Engineering";
+        this.university="Universidad Mayor de San Simon";
+        this.programmingLanguagesList="Java, php, python";
+    }
+    public String myResidenceCity(){
+        return "Currently I live in "+residenceCity+".";
+    }
 }
