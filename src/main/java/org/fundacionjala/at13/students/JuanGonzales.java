8caec3ed182852c0255abdc58042606f336c9885
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-01-21 16:09:46 -0400

    renaming variables

--- src/main/java/org/fundacionjala/at13/students/JuanGonzales.java
+++ src/main/java/org/fundacionjala/at13/students/JuanGonzales.java
@@ -11,23 +11,22 @@ public class JuanGonzales{
 
  */
 
-String name = "Juan Pablo Gonzales Alvarado";
-String nickName = "Juampi";
-int age = 29;
-String career = "Computer Engineer";
-String university = "Juan Misael Saracho";
-String programingLanguage = "Java and Dart";
-String city = "Tarija";
+private String name = "Juan Pablo Gonzales Alvarado";
+    private String nickName = "Juampi";
+    private int age = 29;
+    private String career = "Computer Engineer";
+    private String university = "Juan Misael Saracho";
+    private String programingLanguages = "Java and Dart";
+    private String residenceCity = "Tarija";
 
-
-public String showStudentsInformation() {
-    return "Hello "+"my name is " + name + "\n" +
-            "I like to be called '" + nickName + "\n" +
-            "I am "+ age +" years old" + "\n" +
-            "I am " + career + "\n" +
-            "I study at " + university+ " university \n" +
-            "My programming languages are " + programingLanguage +"\n" +
-            "My residence city " + city;
-}
+    public String showStudentsInformation() {
+        return "Hello "+"my name is " + name + "\n" +
+                "I like to be called '" + nickName + "\n" +
+                "I am "+ age +" years old" + "\n" +
+                "I am " + career + "\n" +
+                "I study at " + university+ " university \n" +
+                "My programming languages are " + programingLanguages +"\n" +
+                "My residence city " + residenceCity;
+    }
 
 }
