a91415749fda6ca2730d8de1d0b795f07f7527af
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-01-21 16:18:19 -0400

    Added atributes to DanielaSantaCruz.java

--- src/main/java/org/fundacionjala/at13/students/DanielaSantaCruz.java
+++ src/main/java/org/fundacionjala/at13/students/DanielaSantaCruz.java
@@ -1,12 +1,22 @@
 package org.fundacionjala.at13.students;
 public class DanielaSantaCruz{
 
-/* Daniela Santa Cruz Andrade
-I like to be called Dani
-I am 22 years old
-Systems engineering
-San Simon University
-Java
-Cochabamba */
+    private String name;
+    private String nickname;
+    private int age;
+    private String career;
+    private String university;
+    private String programmingLanguages;
+    private String recidenceCity;
+    
+    public DanielaSantaCruz(){
+        this.name = "Daniela Santa Cruz Andrade"; 
+        this.nickname = "Dani";
+        this.age = 22;
+        this.career = "Systems engineering";
+        this.university = "UMSS";
+        this.programmingLanguages = "java";
+        this.recidenceCity = "Cochabamba";
+    }
 
 }
