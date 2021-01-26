bc89cf65dc4a648b59247d35683b8563b3aeeb59
SaulCM <saulcaspamiranda@gmail.com>
2021-01-25 22:20:22 -0400

    added method to check only letters and numbers in name

--- src/main/java/org/fundacionjala/at13/authenticator/Auth.java
+++ src/main/java/org/fundacionjala/at13/authenticator/Auth.java
@@ -1,20 +1,26 @@
-org.fundacionjala.at13.authenticator;
+package org.fundacionjala.at13;
 
-public class Auth{​​
+public class Auth.java {
 
-    public static void main(String[] args) {​​
+    public static void main(String[] args) {
+        
+    }
+    public boolean validate(String[]namesList){
+        for(int i=0;i<namesList.length({
 
-    }​​
-    public boolean validate(String[]namesList){​​
-        for(int i=0;i<namesList.length;i++){​​
-            if(namesList[i].length> 6 && namesList[i].length<10)
-            {​​
-            for(int j=0;j<namesList[i].length;j++){​​
-                if(namesList[j].match("[a-z]+") ) // comprobar con ASCII si es minuscula
-            }​​
-            }​​else return false;
-            }​​
+        }
         return res;
-    }​​
-    
-}​​
+    }
+    public static boolean checkNumbersAndLowerCaseLetters(String name){
+        
+        for(int i=0;i<name.length();i++){
+            int asciiCode=(int)name.charAt(i);
+            System.out.println(asciiCode);
+            //conditional if ascii code doesnt belong to lowerCase letters or numbers
+            if(((asciiCode>=97&&asciiCode<=122)||(asciiCode>=48&&asciiCode<=57))==false){
+                return false;
+            }
+        }
+        return true;
+    }
+}
