9746a00d97ab9dd71fc05672ff51bff8991fb9af
Jose Guevara <jguevara@pros.com>
2021-01-27 17:56:55 -0400

    Add capability to read a number for the size of array, and read an array from input

--- src/main/java/org/fundacionjala/at13/authenticator/Auth.java
+++ src/main/java/org/fundacionjala/at13/authenticator/Auth.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 public class Auth {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number:");
-        int input = scanner.nextInt();
-        System.out.println("Input is: " + input);
+        System.out.print("Enter no. of elements you want in array:");
+        int numberOfUser = scanner.nextInt();
+        String usernames[] = new String[numberOfUser];
+        System.out.println("Enter all the usernames:");
+        for (int i = 0; i < numberOfUser; i++) {
+            usernames[i] = scanner.next();
+        }
+        // verify showing in console the array created
+        for (String username: usernames) {
+            System.out.println(username);
+        }
     }
 }
