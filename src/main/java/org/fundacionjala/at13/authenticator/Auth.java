7d8c5b1f29224934a6dff8d4f3ff09f6c7cfa6a2
Jose Guevara <jguevara@pros.com>
2021-01-27 17:48:37 -0400

    Add capability to main class to read integer inputs

--- src/main/java/org/fundacionjala/at13/authenticator/Auth.java
+++ src/main/java/org/fundacionjala/at13/authenticator/Auth.java
@@ -1,5 +1,11 @@
 //package org.fundacionjala.at13;
+import java.util.Scanner;
 
 public class Auth {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int input = scanner.nextInt();
+        System.out.println("Input is: " + input);
+    }
 }
