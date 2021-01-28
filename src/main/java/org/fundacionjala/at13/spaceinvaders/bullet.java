3849a28349ec78646a113d02eac4dff769b43101
edsonarios <edsonrios9@gmail.com>
2021-01-28 16:18:09 -0400

    add method colapsed for the class bullet.java

--- src/main/java/org/fundacionjala/at13/spaceinvaders/bullet.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/bullet.java
@@ -1,14 +1,21 @@
 package org.fundacionjala.at13.spaceinvaders;
 
 public class bullet {
-    public int positionX;
-    public int positionY;
+    public int positionInitialX;
+    public int positionInitialY;
 
     public bullet(int x,int y){
-        positionX = x;
-        positionY = y;
+        positionInitialX = x;
+        positionInitialY = y;
     }
     public void shooter (String type){
-        System.out.println("The "+type+" has fired from "+this.positionX+" "+this.positionY);
+        System.out.println("The "+type+" has fired from "+this.positionInitialX+" "+this.positionInitialY);
     }
+    public boolean colapsed (int x1, int y1, int x2, int y2){
+        if(x1 == x2 && y1 == y2){
+           return true;
+        }
+        return false;
+    }
+
 }
