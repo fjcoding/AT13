dbb7cec9854a7a89ffe6942a8b17ec8b0bdcfe7f
Joel Rojas <jr.bestone17@gmail.com>
2021-01-28 13:30:08 -0400

    Create Spaceship class and main method that shows spaceship initial position 0,0

---
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
@@ -0,0 +1,11 @@
+package org.fundacionjala.at13.spaceinvaders;
+
+public class Spaceship {
+    
+    public static void main(String[] args) {
+        int posX=0;
+        int posY=0;
+        System.out.println("spaceship position: "+posX+","+posY);
+    }
+
+}
