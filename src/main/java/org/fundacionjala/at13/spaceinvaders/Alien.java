cc7d5c53127e03cf7c847c6a772ddc3b064a9314
Jorge Caceres <jcaceresvelasco@gmail.com>
2021-01-28 14:42:53 -0400

    Created New Class Alien. Java Added Method moveRight Added Method moveLeft Added Method moveDown Added Method shoot

---
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Alien.java
@@ -0,0 +1,18 @@
+package org.fundacionjala.at13.spaceinvaders;
+public class Alien { 
+    public Alien(){
+    }  
+    public String moveRight(){
+        return "Alien moved to the rigth";
+    }
+    public String moveLeft(){
+        return "Alien moved to the left";
+    }
+    public String moveDown(){
+        return "Alien moved down";
+    }
+    public String shoot(){
+        return "Alien shooted a bullet";
+    }
+
+}
