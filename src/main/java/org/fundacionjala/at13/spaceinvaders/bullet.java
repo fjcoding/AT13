179b73617ac5857416ee5d5f50028b2a94b83a02
edsonarios <edsonrios9@gmail.com>
2021-01-28 16:05:45 -0400

    create method shooter for the class bullet.java

--- src/main/java/org/fundacionjala/at13/spaceinvaders/bullet.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/bullet.java
@@ -1,3 +1,5 @@
+package org.fundacionjala.at13.spaceinvaders;
+
 public class bullet {
     public int positionX;
     public int positionY;
@@ -5,5 +7,8 @@ public class bullet {
     public bullet(int x,int y){
         positionX = x;
         positionY = y;
+    }
+    public void shooter (String type){
+        System.out.println("The "+type+" has fired from "+this.positionX+" "+this.positionY);
     }
 }
