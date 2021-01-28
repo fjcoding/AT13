fb8de79a0b31d3788bd813aaa7b4a6e974ce0ce9
Raymundo Guaraguara <raymundogs@gmail.com>
2021-01-28 15:59:20 -0400

    Created a new class Matrix.java, to create an array an display the Alien

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Main.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Main.java
@@ -1,13 +1,15 @@
 package org.fundacionjala.at13.spaceinvaders;
-import org.fundacionjala.at13.spaceinvaders.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Space Invaders!!! by AT13"+"\n");
-        Alien Alien1 = new Alien();
-        System.out.println(Alien1.moveLeft()+"\n" );
-        System.out.println(Alien1.moveRight()+"\n" );
-        System.out.println(Alien1.moveDown()+"\n" );
-        System.out.println(Alien1.shoot()+"\n");
-        System.out.println(Alien1.die()+"\n");
+        Matrix spaceNew = new Matrix();
+        // Just a couple of test
+        // Alien Alien1 = new Alien();
+        // System.out.println(Alien1.moveLeft()+"\n" );
+        // System.out.println(Alien1.moveRight()+"\n" );
+        // System.out.println(Alien1.moveDown()+"\n" );
+        // System.out.println(Alien1.shoot()+"\n");
+        // System.out.println(Alien1.die()+"\n");
     }
 }
