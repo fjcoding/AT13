4eeb2a38487e31c4d54ae8f799e0c85920bb8a8e
Joel Rojas <jr.bestone17@gmail.com>
2021-01-28 13:46:29 -0400

    Add Spaceship cosntructor and get methods

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
@@ -3,15 +3,32 @@ package org.fundacionjala.at13.spaceinvaders;
 import java.util.Scanner;
 
 public class Spaceship {
+
+    private int posX;
+    private int posY;
     
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter initial position x: ");
-        int posX=scanner.nextInt();
+        int posXInitial=scanner.nextInt();
         System.out.print("Enter initial position y: ");
-        int posY=scanner.nextInt();;
-        System.out.println("spaceship position: "+posX+","+posY);
+        int posYInitial=scanner.nextInt();;
+        Spaceship spaceship=new Spaceship(posXInitial, posYInitial);
+        System.out.println("spaceship position: "+spaceship.getPosX()+","+spaceship.getPosY());
         scanner.close();
+    }
+
+    public Spaceship(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+    
+    public int getPosX() {
+        return this.posX;
+    }
+
+    public int getPosY() {
+        return this.posY;
     }
 
 }
