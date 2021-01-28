731556bd08f7996b4efa0bfb67f0d5c5a8c51708
Joel Rojas <jr.bestone17@gmail.com>
2021-01-28 13:37:25 -0400

    Add functionality to enter initial position of spaceship

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
@@ -1,11 +1,17 @@
 package org.fundacionjala.at13.spaceinvaders;
 
+import java.util.Scanner;
+
 public class Spaceship {
     
     public static void main(String[] args) {
-        int posX=0;
-        int posY=0;
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter initial position x: ");
+        int posX=scanner.nextInt();
+        System.out.print("Enter initial position y: ");
+        int posY=scanner.nextInt();;
         System.out.println("spaceship position: "+posX+","+posY);
+        scanner.close();
     }
 
 }
