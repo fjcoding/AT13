11d60b704dd980fd0e8ee0f17ceb7f0e53697ca7
edsonarios <edsonrios9@gmail.com>
2021-01-28 16:38:48 -0400

    change name colapse to isCollapsedBullet in class bullet.java

--- src/main/java/org/fundacionjala/at13/spaceinvaders/bullet.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/bullet.java
@@ -1,21 +1,29 @@
 package org.fundacionjala.at13.spaceinvaders;
 
-public class bullet {
+public class bullet{
     public int positionInitialX;
     public int positionInitialY;
+    public boolean bulletExist;
 
     public bullet(int x,int y){
         positionInitialX = x;
         positionInitialY = y;
+        bulletExist = true;
     }
-    public void shooter (String type){
-        System.out.println("The "+type+" has fired from "+this.positionInitialX+" "+this.positionInitialY);
+    public void shooter(String type){
+        if(bulletExist){
+            System.out.println("The " + type + " has fired from " + this.positionInitialX + " " + this.positionInitialY);
+            bulletExist = false;
+        }else{
+            System.out.println("the " + type + " cannot fired");
+        }
     }
-    public boolean colapsed (int x1, int y1, int x2, int y2){
+    public boolean isCollapsedBullet(int x1, int y1, int x2, int y2){
         if(x1 == x2 && y1 == y2){
-           return true;
+            System.out.println("The bullet has collapsed");
+            bulletExist = true;
+            return true;
         }
         return false;
     }
-
 }
