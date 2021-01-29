d082c0e1341ebf7e625343d9b9b8ce5734a62a35
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-01-29 10:21:46 -0400

    created the createdSpace method

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Space.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Space.java
@@ -1,4 +1,15 @@
 package org.fundacionjala.at13.spaceinvaders;
 
 public class Space {
+    private static final int SPACE_SIZE=50;
+
+    static String [][] space = new String[SPACE_SIZE][SPACE_SIZE];
+    private static void createdSpace(){
+        for (int i = 0; i < space[0].length ; i++) {
+            for (int j = 0; j <space[0].length-1 ; j++) {
+                space[i][j] = ".";
+            }
+        }
+    }
+
 }
