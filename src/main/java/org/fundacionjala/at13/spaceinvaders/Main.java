55ca244acb748766b375a7035768f68ae862afc1
cristianfchq <cristian.fchq@gmail.com>
2021-01-29 16:01:27 -0400

    remove the instance methods in the Main class

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Main.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Main.java
@@ -5,11 +5,8 @@ import org.fundacionjala.at13.spaceinvaders.*;
 public class Main {
 
     public static void main(String[] args) {
+        
         System.out.println("Space Invaders!!! by AT13");
 
-        Space space = new Space();
-
-        space.createdSpace();
-        space.showSpace();;
     }
 }
