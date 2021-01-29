e11bfe87e11908bba6dba34eab8378b39a9b8551
cristianfchq <cristian.fchq@gmail.com>
2021-01-29 12:54:02 -0400

    create a method showSpace on the file Space.java

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Space.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Space.java
@@ -8,6 +8,13 @@ public class Space{
         for (int i = 0; i < space.length ; i++) {
             for (int j = 0; j <space[i].length ; j++) {
                 space[i][j] = ".";
+            }
+        }
+    }
+
+    public static void showSpace(){
+        for (int i = 0; i < space.length ; i++) {
+            for (int j = 0; j <space[i].length ; j++) {
                 System.out.print(space[i][j]);
             }
             System.out.println();
