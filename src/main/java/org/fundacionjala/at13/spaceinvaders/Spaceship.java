10322acd1f196a7c80506bcac5ecd3ca23cb75ff
Joel Rojas <jr.bestone17@gmail.com>
2021-01-29 14:12:28 -0400

    Declare UPPER_LIMIT and LOWER_LIMIT and implement getter and setter methods

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
@@ -4,11 +4,40 @@ public class Spaceship {
 
     private int posX;
     private int posY;
+    private final int LOWER_lIMIT=0;
+    private final int UPPER_lIMIT=50;
     
 
     public Spaceship(int posX, int posY) {
         this.posX = posX;
         this.posY = posY;
+    }
+
+
+    public int getPosX() {
+        return this.posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return this.posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+
+    public int getLOWER_lIMIT() {
+        return this.LOWER_lIMIT;
+    }
+
+
+    public int getUPPER_lIMIT() {
+        return this.UPPER_lIMIT;
     }
     
 }
