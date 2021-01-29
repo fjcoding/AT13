bff21ee00a326366b5150f8ac68b96e415276643
Joel Rojas <jr.bestone17@gmail.com>
2021-01-29 13:57:59 -0400

    Create Spaceship.java and its constructor method

---
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
@@ -0,0 +1,14 @@
+package org.fundacionjala.at13.spaceinvaders;
+
+public class Spaceship {
+
+    private int posX;
+    private int posY;
+    
+
+    public Spaceship(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+    
+}
