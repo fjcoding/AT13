125be9f616896abc6e645a69198979c015e21f30
JessickaMoya <jessicka.moya@gmail.com>
2021-01-29 15:26:09 -0400

    Make unit test itShouldMoveToRight() in SpaceshipTest.java

--- src/test/java/org/fundacionjala/at13/spaceinvaders/SpaceshipTest.java
+++ src/test/java/org/fundacionjala/at13/spaceinvaders/SpaceshipTest.java
@@ -1,5 +1,16 @@
 package org.fundacionjala.at13.spaceinvaders;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class SpaceshipTest{
+
+    @Test
+    public void itShouldMoveToRight(){
+        Spaceship spaceship = new Spaceship(10,10);
+        spaceship.moveToRight();
+        int posY = spaceship.getPosY();
+        assertEquals(11, posY);
+    }
 
 }
