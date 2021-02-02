a90a659023451abbf7301e53e938aac752b68ec9
cristianfchq <cristian.fchq@gmail.com>
2021-02-01 23:13:31 -0400

    add comment before all the methods with your description

--- src/main/java/org/fundacionjala/at13/students/ZCristianChoque.java
+++ src/main/java/org/fundacionjala/at13/students/ZCristianChoque.java
@@ -35,7 +35,6 @@ public class ZCristianChoque {
     /** DESCRIPTION.
      * A default constructor for the class
      */
-
     public ZCristianChoque() {
         this.name = "Cristian Felix Choque Quispe";
         this.nickName = "Cristian";
@@ -45,14 +44,24 @@ public class ZCristianChoque {
         this.programingLanguages = "c++";
         this.residenceCity = "La Paz";
     }
-    
+    /** DESCRIPTION.
+     * method for to get residence of city
+     * @return a string with the date of the residence
+     */
     public String getResidenceCity(){
         return "Currently I live in "+residenceCity+".";
     }
+    /** DESCRIPTION.
+     * method for to get or to show nick name
+     * @return a string with the nickName
+     */
     public String ShowNickName(){
         return "I like to be called "+this.nickName;
     }
-
+    /** DESCRIPTION.
+     * method for to get all information of the Student
+     * @return a string with the all dates of the Student
+     */
     public String showStudentsInformation() {
         return "Hello "+"my name is " + name + "\n" +
                 "I like to be called '" + nickName + "\n" +
@@ -63,23 +72,45 @@ public class ZCristianChoque {
                 "My residence city " + residenceCity +"\n" +
                 "I have born in the year "+(2021-age)+".";
     }
+    /** DESCRIPTION.
+     * method for to get name
+     * @return a string with the name of the student
+     */
     public String ShowName(){
         return "Hi, my name is "+this.name;
     }
-
+    /** DESCRIPTION.
+     * method for to get career
+     * @return a string with the career of the student
+     */
     public String getCareer(){
 		return "I have studied" + this.career;
     }
+    /** DESCRIPTION.
+     * method for to get favorite programing language of the student 
+     * @return a string with the favorite programing language of the student
+     */
     public String showProgramingLanguage() {
         return "I like the programming languages " + this.programingLanguages;
     }
+    /** DESCRIPTION.
+     * method for to get age of student
+     * @return a string with the age of the student
+     */
     public String ShowAge(){
         return "Hi, my name is "+this.age;
     } 
+    /** DESCRIPTION.
+     * method for to get university where studied
+     * @return a string with the university of the student
+     */
     public String ShowUniversity(){
         return "I studied at "+this.university;
     }
-
+    /** DESCRIPTION.
+     * method for to get year birth of the student
+     * @return a string with the year that born
+     */
     public String showYearBirth(){
         return "I have born in the year "+(2021-age)+".";
     }
