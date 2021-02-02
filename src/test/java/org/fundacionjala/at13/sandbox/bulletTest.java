091a6bb32474e2846f777caa4fe502b7ae4741ea
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-02-02 15:56:48 -0400

    delete bullet.java and move Bullet.java into test/..../sanbox file

--- src/test/java/org/fundacionjala/at13/sandbox/bulletTest.java
+++ src/test/java/org/fundacionjala/at13/sandbox/bulletTest.java
@@ -1,5 +1,4 @@
-
-import org.fundacionjala.at13.spaceinvaders.bullet;
+import org.fundacionjala.at13.spaceinvaders.Bullet;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -7,22 +6,22 @@ public class bulletTest{
 
     @Test
     public void itShooterAllien() {
-        bullet bullet = new bullet(1,2);
+        Bullet bullet = new Bullet(1,2);
         assertEquals("The Allien has fired from 1 2", bullet.shooter("Allien"));
     }
     @Test
     public void itShooterSpaceship() {
-        bullet bullet = new bullet(3,2);
+        Bullet bullet = new Bullet(3,2);
         assertEquals("The Spaceship has fired from 3 2", bullet.shooter("Spaceship"));
     }
     @Test
     public void itAlliensBulletCollapsedWithSpaceshipsBullet() {
-        bullet bullet = new bullet(3,2);
+        Bullet bullet = new Bullet(3,2);
         assertEquals(true, bullet.isCollapsedBullet(1, 2, 1, 2));
     }
     @Test
     public void itAlliensBulletNotCollapsedWithSpaceshipsBullet() {
-        bullet bullet = new bullet(3,2);
+        Bullet bullet = new Bullet(3,2);
         assertEquals(false, bullet.isCollapsedBullet(1, 3, 1, 2));
-    }
+    } 
 }
