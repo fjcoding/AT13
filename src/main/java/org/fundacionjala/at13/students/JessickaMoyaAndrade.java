26446e96cbfced5918e30fdb56ba80674013a707
JessickaMoya <jessicka.moya@gmail.com>
2021-02-02 01:11:20 -0400

    Fix checkstyle errors about magic numbers, name of methods, trailing spaces

--- src/main/java/org/fundacionjala/at13/students/JessickaMoyaAndrade.java
+++ src/main/java/org/fundacionjala/at13/students/JessickaMoyaAndrade.java
@@ -2,25 +2,27 @@
 package org.fundacionjala.at13.students;
 
 public class JessickaMoyaAndrade {
+    public static final int CURRENT_YEAR = 2021;
+    public static final int CURRENT_AGE = 23;
     private String name = "Jessicka Moya Andrade";
     private String nickName = "Jess";
-    private int age = 23;
+    private int age = CURRENT_AGE;
     private String career = "Systems Engineering";
     private String university = "UMSS";
     private String programingLanguages = "java, php";
     private String residenceCity = "Cochabamba, Cochabamba";
 
     public String getCareer() {
-		return "I have studied" + this.career;
+        return "I have studied" + this.career;
 	}
 
     public String getResidenceCity() {
         return "Currently I live in " + residenceCity + ".";
     }
-    public String ShowNickName() {
+    public String showNickName() {
         return "I like to be called " + this.nickName;
     }
-    
+
     public String showStudentsInformation() {
         return "Hello " + "my name is " + name + "\n"
                 + "I like to be called '" + nickName + "\n"
@@ -29,21 +31,21 @@ public class JessickaMoyaAndrade {
                 + "I study at " + university + " university \n"
                 + "My programming languages are " + programingLanguages + "\n"
                 + "My residence city " + residenceCity + "\n"
-                + "I have born in the year " + (2021 - age) + ".";
+                + "I have born in the year " + (CURRENT_YEAR - age) + ".";
     }
-    public String ShowName() {
+    public String showName() {
         return "Hi, my name is " + this.name;
     }
     public String showProgramingLanguage() {
         return "I like the programming languages " + this.programingLanguages;
     }
-    public String ShowAge() {
+    public String showAge() {
         return "Hi, my name is " + this.age;
-    } 
-    public String ShowUniversity() {
+    }
+    public String showUniversity() {
         return "I studied at " + this.university;
     }
     public String showYearBirth() {
-        return "I have born in the year " + (2021 - age) + ".";
+        return "I have born in the year " + (CURRENT_YEAR - age) + ".";
     }
 }
