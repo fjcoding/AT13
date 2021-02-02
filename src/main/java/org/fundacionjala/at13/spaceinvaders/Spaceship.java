418cd329ecd2f10948d7b993459f658bf367644f
Joel Rojas <jr.bestone17@gmail.com>
2021-02-02 14:11:07 -0400

    add comments to all methods

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
@@ -11,50 +11,86 @@ public class Spaceship {
         this.posX = pX;
         this.posY = pY;
     }
+    /**
+     * Description: Getter method.
+     * @return an Integer with position X of Spaceship.
+     */
     public int getPosX() {
         return this.posX;
     }
+
+    /**
+     * Description: Getter method.
+     * @return an Integer with position Y of Spaceship.
+     */
     public int getPosY() {
         return this.posY;
     }
+
+    /**
+     * Description: Change position X for a new one.
+     */
     public void setPosX(final int pX) {
         if (pX < lowerLimit) {
             this.posX = lowerLimit;
         } else if (pX > upperLimit - 1) {
             this.posX = (upperLimit - 1);
         } else {
-            this.posX = pX;
+            this.posX = posX;
         }
     }
+
+    /**
+     * Description: Change position Y for a new one.
+     */
     public void setPosY(final int pY) {
         if (pY < lowerLimit) {
             this.posY = lowerLimit;
         } else if (pY > upperLimit) {
             this.posY = upperLimit - 1;
         } else {
-            this.posY = pY;
+            this.posY = posY;
         }
     }
 
+    /**
+     * Description: Getter method.
+     * @return an Integer with lower limit that Spaceship can move.
+     */
     public int getLowerLimint() {
         return this.lowerLimit;
     }
 
+    /**
+     * Description: Getter method.
+     * @return an Integer with upper limit that Spaceship can move.
+     */
     public int getUpperLimint() {
         return this.upperLimit;
     }
 
+    /**
+     * Description: Add one position to posY if new psition is between limits.
+     */
     public void moveToRight() {
         if (this.posY < upperLimit) {
             this.posY += 1;
         }
     }
 
+    /**
+     * Description: Less one position to posY if new psition is between limits..
+     */
     public void moveToLeft() {
         if (this.posY > lowerLimit) {
             this.posY -= 1;
         }
     }
+
+    /**
+     * Description: Getter method.
+     * @return an String with position x and y where it shooted.
+     */
     public String shoot() {
         return ">>>>> piw <<<<< shoot position: " + getPosX() + "," + getPosY();
     }
