624f0cead6f38db0506f9df676a2b45a3cca8f70
Raymundo Guaraguara <raymundogs@gmail.com>
2021-02-02 14:13:24 -0400

    Fix checkstyles errors in files Matrix.java and Alien.java
    Change data type from int to final int.
    Add function description on all Alien and Matrix methods.
    Add space before {.
    Add space before and after =.

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Alien.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Alien.java
@@ -1,46 +1,86 @@
 package org.fundacionjala.at13.spaceinvaders;
-public class Alien { 
-    //The variables are named after the x and y axis of the cartesian plane
-    int xPos;
-    int yPos;
-    public Alien(){
+public class Alien {
+    private int xPos;
+    private int yPos;
+    public Alien() {
         this.xPos = 0;
         this.yPos = 0;
     }
 
+    /**
+     * Description: Function that gets xPos.
+     *
+     * @return xPos an int that shows x position.
+     */
     public int getXPos() {
         return xPos;
     }
 
-    public void setXPos(int x) {
+    /**
+     * Description: Function that sets xPos.
+     */
+    public void setXPos(final int x) {
         this.xPos = x;
     }
 
+    /**
+     * Description: Function that gets yPos.
+     *
+     * @return yPos an int that shows y position.
+     */
     public int getYPos() {
         return yPos;
     }
 
-    public void setYPos(int y) {
+    /**
+     * Description: Function that sets yPos.
+     */
+    public void setYPos(final int y) {
         this.yPos = y;
     }
 
-    public int moveRight(){
+    /**
+     * Description: Function moves alien right.
+     *
+     * @return int that represents movement.
+     */
+    public int moveRight() {
         return 1;
     }
 
-    public int moveLeft(){
+    /**
+     * Description: Function moves alien left.
+     *
+     * @return int that represents movement.
+     */
+    public int moveLeft() {
         return 1;
     }
 
-    public int moveDown(){
+    /**
+     * Description: Function moves alien down.
+     *
+     * @return int that represents movement.
+     */
+    public int moveDown() {
         return 1;
     }
 
-    public String shoot(){
+    /**
+     * Description: Function shoots a bullet.
+     *
+     * @return string that represents a shoot.
+     */
+    public String shoot() {
         return "Alien shooted a bullet";
     }
 
-    public Boolean die(){
+    /**
+     * Description: Function makes alien die.
+     *
+     * @return boolean that represents alien's dead.
+     */
+    public Boolean die() {
         return false;
     }
 }
--- src/main/java/org/fundacionjala/at13/spaceinvaders/Matrix.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Matrix.java
@@ -1,33 +1,34 @@
 package org.fundacionjala.at13.spaceinvaders;
-import org.fundacionjala.at13.spaceinvaders.Alien;
 
 public class Matrix {
     public static final int MATRIX_HEIGHT = 3;
     public static final int MATRIX_WIDTH = 3;
-    public int[][] space;
-    public Alien firstAlien = new Alien();
+    private int[][] space;
+    private Alien firstAlien = new Alien();
 
-    public Matrix(){
+    public Matrix() {
         this.space = new int[MATRIX_HEIGHT][MATRIX_WIDTH];
-        //fillMatrix();
-        //showMatrix();
-        //firstAlien.moveRight();
-        //fillMatrix();
-        //showMatrix();
     }
 
+    /**
+     * Description: The function creates an array of 1 and 0s.
+     */
     public void fillMatrix() {
         for (int i = 0; i < MATRIX_HEIGHT; i++) {
             for (int j = 0; j < MATRIX_WIDTH; j++) {
-                if ((i == firstAlien.getYPos()) && (j == firstAlien.getXPos())) {
-                    space[i][j]=1;
+                if ((i == firstAlien.getYPos())
+                 && (j == firstAlien.getXPos())) {
+                    space[i][j] = 1;
                 } else {
-                    space[i][j]=0;
+                    space[i][j] = 0;
                 }
             }
         }
     }
 
+    /**
+     * Description: The function displays an array.
+     */
     public void showMatrix() {
         for (int i = 0; i < MATRIX_HEIGHT; i++) {
             for (int j = 0; j < MATRIX_WIDTH; j++) {
