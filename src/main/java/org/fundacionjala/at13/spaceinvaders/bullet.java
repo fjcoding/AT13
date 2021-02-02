bffd2a52a786607035288d84357ab65fde911c41
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-02-02 00:45:18 -0400

    Solve checkstyle errors in the classes DanielaSantaCruz.java and bullet.java

--- src/main/java/org/fundacionjala/at13/spaceinvaders/bullet.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/bullet.java
@@ -1,29 +1,70 @@
 package org.fundacionjala.at13.spaceinvaders;
+public class Bullet {
+    private int positionInitialX;
+    private int positionInitialY;
+    private boolean bulletExist;
 
-public class bullet{
-    public int positionInitialX;
-    public int positionInitialY;
-    public boolean bulletExist;
-
-    public bullet(int x, int y){
+    public Bullet(int x, int y) {
         positionInitialX = x;
         positionInitialY = y;
         bulletExist = true;
     }
-    public String shooter(String type){
-        if(bulletExist){
+
+    /**
+     * Description: The function displays bullet's position initial x.
+     *
+     * @return a int that shows bullet's position initial x.
+     */
+    public int getPositionX() {
+        return this.positionInitialX;
+    }
+
+    /**
+     * Description: The function displays bullet's position initial y.
+     *
+     * @return a int that shows bullet's position initial y.
+     */
+    public int getPositionY() {
+        return this.positionInitialY;
+    }
+
+    /**
+     * Description: The function verifies if the bullet exists.
+     *
+     * @return a boolean that verifies if the bullet exists.
+     */
+    public boolean getBulletExist() {
+        return this.bulletExist;
+    }
+
+    /**
+     * Description: The function displays a bullet shoot.
+     *
+     * @return a String that shoots a bullet.
+     */
+    public String shooter(String type) {
+        if (bulletExist) {
             bulletExist = false;
-            return "The " + type + " has fired from " + this.positionInitialX + " " + this.positionInitialY;
-        }else{
+            return "The " + type + " has fired from " + this.positionInitialX
+            + " " + this.positionInitialY;
+        } else {
             return "the " + type + " cannot fired";
         }
     }
-    public boolean isCollapsedBullet(int positionX1, int positionY1, int positionX2, int positionY2){
-        if(positionX1 == positionX2 && positionY1 == positionY2){
+
+    /**
+     * Description: The function verifies if the bullet collapses.
+     *
+     * @return a boolean that verifies if the bullet collapses.
+     */
+    public boolean isCollapsedBullet(int positionX1, int positionY1,
+                          int positionX2, int positionY2) {
+        if (positionX1 == positionX2 && positionY1 == positionY2) {
             System.out.println("The bullet has collapsed");
             bulletExist = true;
             return true;
         }
         return false;
     }
+
 }
