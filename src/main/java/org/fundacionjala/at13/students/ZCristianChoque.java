e418d462044c1e0b98368b88aa99f51112da8692
cristianfchq <cristian.fchq@gmail.com>
2021-02-01 23:26:38 -0400

    all + is follow space

--- src/main/java/org/fundacionjala/at13/students/ZCristianChoque.java
+++ src/main/java/org/fundacionjala/at13/students/ZCristianChoque.java
@@ -67,14 +67,14 @@ public class ZCristianChoque {
      * @return a string with the all dates of the Student
      */
     public String showStudentsInformation() {
-        return "Hello " + "my name is " + name + "\n" +
-                "I like to be called '" + nickName + "\n" +
-                "I am " + age +" years old" + "\n" +
-                "I am " + career + "\n" +
-                "I study at " + university + " university \n" +
-                "My programming languages are " + programingLanguages + "\n" +
-                "My residence city " + residenceCity + "\n" +
-                "I have born in the year " + (2021-age)+".";
+        return "Hello " + "my name is " + name + "\n" + 
+                "I like to be called '" + nickName + "\n" + 
+                "I am " + age +" years old" + "\n" + 
+                "I am " + career + "\n" + 
+                "I study at " + university + " university \n" + 
+                "My programming languages are " + programingLanguages + "\n" + 
+                "My residence city " + residenceCity + "\n" + 
+                "I have born in the year " + (2021-age) + ".";
     }
     /** DESCRIPTION.
      * method for to get name
