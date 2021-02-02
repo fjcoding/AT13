60ca1d9233b2a33ad0bfb0045bf67ce5e52be634
Joel Rojas <jr.bestone17@gmail.com>
2021-02-01 23:10:17 -0400

    Space between () and { of methods, between =,+ and attributes and vice versa, changes in method names

--- src/main/java/org/fundacionjala/at13/students/JoelRojas.java
+++ src/main/java/org/fundacionjala/at13/students/JoelRojas.java
@@ -3,34 +3,34 @@ package org.fundacionjala.at13.students;
 public class JoelRojas{
 
     private String name;
-    private String nickName;
+    private String nickname;
     private int age;
     private String career;
     private String university;
     private String programingLanguages;
     private String residenceCity;
 
-    public JoelRojas(){
-        this.name="Joel Rodrigo Rojas Roman";
-        this.nickName="Joel";
-        this.age=27;
-        this.career="Systems Engineering";
-        this.university="UMSS";
-        this.programingLanguages="Java";
-        this.residenceCity="Quillacollo, Cochabamba";
+    public JoelRojas() {
+        this.name = "Joel Rodrigo Rojas Roman";
+        this.nickname = "Joel";
+        this.age = 27;
+        this.career = "Systems Engineering";
+        this.university = "UMSS";
+        this.programingLanguages = "Java";
+        this.residenceCity = "Quillacollo, Cochabamba";
     }
 
-    public String ShowNickName(){
-        return "I like to be called "+this.nickName;
+    public String showNickname() {
+        return "I like to be called " + this.nickname;
     }
 
-    public String getResidenceCity(){
-        return "Currently I live in "+residenceCity+".";
+    public String getResidenceCity() {
+        return "Currently I live in " + residenceCity + ".";
     }
     
     public String showStudentsInformation() {
         return "Hello "+"my name is " + name + "\n" +
-                "I like to be called '" + nickName + "\n" +
+                "I like to be called '" + nickname + "\n" +
                 "I am "+ age +" years old" + "\n" +
                 "I am " + career + "\n" +
                 "I study at " + university+ " university \n" +
@@ -38,22 +38,28 @@ public class JoelRojas{
                 "My residence city " + residenceCity +"\n" +
                 "I have born in the year "+(2021-age)+".";
     }
-    public String ShowName(){
-        return "Hi, my name is "+this.name;
+
+    public String showName() {
+        return "Hi, my name is " + this.name;
     }
-    public String getCareer(){
+
+    public String getCareer() {
 		return "I have studied" + this.career;
     }
+
     public String showProgramingLanguage() {
         return "I like the programming languages " + this.programingLanguages;
     }
-    public String ShowAge(){
-        return "Hi, my name is "+this.age;
+
+    public String showAge() {
+        return "Hi, my name is " + this.age;
     } 
-    public String ShowUniversity(){
-        return "I studied at "+this.university;
+
+    public String ShowUniversity() {
+        return "I studied at " + this.university;
     }
-    public String showYearBirth(){
-        return "I have born in the year "+(2021-age)+".";
+
+    public String showYearBirth() {
+        return "I have born in the year " + (2021-age) + ".";
     }
 }
