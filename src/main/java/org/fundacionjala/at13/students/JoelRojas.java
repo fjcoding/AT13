5280a598cf23905b46aa27f185c624ba189f9543
Joel Rojas <jr.bestone17@gmail.com>
2021-02-02 00:11:19 -0400

    Delete some blank spaces and comment attributes and methods

--- src/main/java/org/fundacionjala/at13/students/JoelRojas.java
+++ src/main/java/org/fundacionjala/at13/students/JoelRojas.java
@@ -1,65 +1,113 @@
 package org.fundacionjala.at13.students;
 
-public class JoelRojas{
+public class JoelRojas {
 
+    /** Description: Student's name.*/
     private String name;
+    /** Description: Student's nickname.*/
     private String nickname;
+    /** Description: Student's age.*/
     private int age;
+    /** Description: Student's career.*/
     private String career;
+    /** Description: Student's university.*/
     private String university;
+    /** Description: Student's programing languages.*/
     private String programingLanguages;
+    /** Description: Student's residence city.*/
     private String residenceCity;
+    /** Description: Student's current age.*/
+    private static final int CURRENT_JOEL_AGE = 27;
+    /** Description: Student's current year.*/
+    private static final int CURRENT_YEAR = 2021;
 
+    /** Description: Constructor of JoelRojas class.*/
     public JoelRojas() {
         this.name = "Joel Rodrigo Rojas Roman";
         this.nickname = "Joel";
-        this.age = 27;
+        this.age = CURRENT_JOEL_AGE;
         this.career = "Systems Engineering";
         this.university = "UMSS";
         this.programingLanguages = "Java";
         this.residenceCity = "Quillacollo, Cochabamba";
     }
 
+    /**
+     * Description: The function shows student's nickname.
+     * @return a String with a nickname's presentation.
+     */
     public String showNickname() {
         return "I like to be called " + this.nickname;
     }
 
+    /**
+     * Description: The function shows student's residence city.
+     * @return a String with a residence city's presentation.
+     */
     public String getResidenceCity() {
         return "Currently I live in " + residenceCity + ".";
     }
-    
+
+    /**
+     * Description: The function shows student's information.
+     * @return a String with a complete information's presentation.
+     */
     public String showStudentsInformation() {
-        return "Hello "+"my name is " + name + "\n" +
-                "I like to be called '" + nickname + "\n" +
-                "I am "+ age +" years old" + "\n" +
-                "I am " + career + "\n" +
-                "I study at " + university+ " university \n" +
-                "My programming languages are " + programingLanguages +"\n" +
-                "My residence city " + residenceCity +"\n" +
-                "I have born in the year "+(2021-age)+".";
+        return "Hello " + "my name is " + name + "\n"
+                + "I like to be called " + nickname + "\n"
+                + "I am " + age + " years old" + "\n"
+                + "I am " + career + "\n"
+                + "I study at " + university + " university \n"
+                + "My programming languages are " + programingLanguages + "\n"
+                + "My residence city " + residenceCity + "\n"
+                + "I have born in the year " + (CURRENT_YEAR - age) + ".";
     }
 
+    /**
+     * Description: The function shows student's name.
+     * @return a String with a greeting and name's presentation.
+     */
     public String showName() {
         return "Hi, my name is " + this.name;
     }
 
+    /**
+     * Description: The function shows student's career.
+     * @return a String with a career's presentation.
+     */
     public String getCareer() {
-		return "I have studied" + this.career;
+        return "I have studied" + this.career;
     }
 
+    /**
+     * Description: The function shows what programming languages student likes.
+     * @return a String with a programming languages's presentation.
+     */
     public String showProgramingLanguage() {
         return "I like the programming languages " + this.programingLanguages;
     }
 
+    /**
+     * Description: The function shows student's age.
+     * @return a String with a age's presentation.
+     */
     public String showAge() {
         return "Hi, my name is " + this.age;
-    } 
+    }
 
-    public String ShowUniversity() {
+    /**
+     * Description: The function shows student's university.
+     * @return a String with a university's presentation.
+     */
+    public String showUniversity() {
         return "I studied at " + this.university;
     }
 
+    /**
+     * Description: The function shows year that student was born.
+     * @return a String with a year birth's presentation.
+     */
     public String showYearBirth() {
-        return "I have born in the year " + (2021-age) + ".";
+        return "I have born in the year " + (CURRENT_YEAR - age) + ".";
     }
 }
