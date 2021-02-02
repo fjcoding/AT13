ca582b4a4027d790c8de64757d830bc8c2f2c6dc
Gustavo Huanca <gustavozha@gmail.com>
2021-02-02 13:07:04 -0400

    Fix some error in Spaceship following checkstyle

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
@@ -4,64 +4,58 @@ public class Spaceship {
 
     private int posX;
     private int posY;
-    private final int LOWER_lIMIT=0;
-    private final int UPPER_lIMIT=50;
-    
+    private final int lowerLimit = 0;
+    private final int upperLimit = 50;
 
     public Spaceship(int posX, int posY) {
         this.posX = posX;
         this.posY = posY;
     }
-
-
     public int getPosX() {
         return this.posX;
     }
-
     public int getPosY() {
         return this.posY;
     }
-
     public void setPosX(int posX) {
-        if(posX<LOWER_lIMIT){
-            this.posX = LOWER_lIMIT;
-        }else if(posX>UPPER_lIMIT-1){
-            this.posX = (UPPER_lIMIT-1);
-        }else{
+        if (posX < lowerLimit) {
+            this.posX = lowerLimit;
+        } else if (posX > upperLimit - 1) {
+            this.posX = (upperLimit - 1);
+        } else {
             this.posX = posX;
         }
     }
-
     public void setPosY(int posY) {
-        if(posY<LOWER_lIMIT){
-            this.posY = LOWER_lIMIT;
-        }else if(posY>UPPER_lIMIT){
-            this.posY = UPPER_lIMIT-1;
-        }else{
+        if (posY < lowerLimit) {
+            this.posY = lowerLimit;
+        } else if (posY > upperLimit) {
+            this.posY = upperLimit - 1;
+        } else {
             this.posY = posY;
         }
     }
 
-    public int getLOWER_lIMIT() {
-        return this.LOWER_lIMIT;
+    public int getLowerLimint() {
+        return this.lowerLimit;
     }
 
-    public int getUPPER_lIMIT() {
-        return this.UPPER_lIMIT;
+    public int getUpperLimint() {
+        return this.upperLimit;
     }
 
-    public void moveToRight(){
-        if (this.posY<UPPER_lIMIT) {
-            this.posY+=1;
+    public void moveToRight() {
+        if (this.posY < upperLimit) {
+            this.posY += 1;
         }
     }
 
-    public void moveToLeft(){
-        if (this.posY>LOWER_lIMIT) {
-            this.posY-=1;           
+    public void moveToLeft() {
+        if (this.posY > lowerLimit) {
+            this.posY -= 1;
         }
     }
-    public String shoot(){
-        return ">>>>>>> piw <<<<<<< shoot position: "+getPosX()+","+getPosY();
+    public String shoot() {
+        return ">>>>> piw <<<<< shoot position: " + getPosX() + "," + getPosY();
     }
 }
