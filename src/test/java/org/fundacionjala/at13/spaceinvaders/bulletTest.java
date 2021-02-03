3424acfe09b6f7a91ee2870bf34697de0219eed7
Paola-Xaq <paola.xaq@gmail.com>
2021-02-03 14:49:46 -0400

    Modified methods name and add test for Bullet.class
    Modified methods name in class Bullet.java
    
    Add Test for bulletTest.java
    	Test for positionX
    	Test for positionY
    	Test for colapsedBullet
    	And correct the other tests.

--- src/test/java/org/fundacionjala/at13/spaceinvaders/bulletTest.java
+++ src/test/java/org/fundacionjala/at13/spaceinvaders/bulletTest.java
@@ -5,23 +5,50 @@ import org.junit.Test;
 public class bulletTest{
 
     @Test
+    public void itGetBulletPositiionInX() {
+        Bullet Bullet = new Bullet("Alien",5,6);
+        assertEquals(5,Bullet.getPositionX());
+    }
+    @Test
+    public void itGetBulletPositiionInY() {
+        Bullet Bullet = new Bullet("Alien",5,6);
+        assertEquals(6,Bullet.getPositionY());
+    }
+    @Test
+    public void ifBulletNotExistWhileNotShooting() {
+        Bullet Bullet = new Bullet("Spaceship",3,3);
+        assertEquals(false,Bullet.getBulletExist());
+    }
+    @Test
+    public void ifBulletExistWhileShooting() {
+        Bullet Bullet = new Bullet("Spaceship",3,3);
+        Bullet.itShooting();
+        assertEquals(true,Bullet.getBulletExist());
+    }
+    @Test
     public void itShooterAllien() {
-        Bullet bullet = new Bullet(1,2);
-        assertEquals("The Allien has fired from 1 2", bullet.shooter("Allien"));
+        Bullet Bullet = new Bullet("Alien",1,2);
+        assertEquals("The Alien has fired from 1 2", Bullet.itShooting());
     }
     @Test
     public void itShooterSpaceship() {
-        Bullet bullet = new Bullet(3,2);
-        assertEquals("The Spaceship has fired from 3 2", bullet.shooter("Spaceship"));
+        Bullet Bullet = new Bullet("Spaceship",3,2);
+        assertEquals("The Spaceship has fired from 3 2", Bullet.itShooting());
+    }
+    @Test
+    public void ifAlienCantShootBecauseAlreadyShoot() {
+        Bullet Bullet = new Bullet("Alien",3,2);
+        Bullet.itShooting();
+        assertEquals("The Alien cannot fired.", Bullet.itShooting());
     }
     @Test
     public void itAlliensBulletCollapsedWithSpaceshipsBullet() {
-        Bullet bullet = new Bullet(3,2);
-        assertEquals(true, bullet.isCollapsedBullet(1, 2, 1, 2));
+        Bullet Bullet = new Bullet("Spaceship",3,2);
+        assertEquals(true, Bullet.isCollapsedBullet(1, 2, 1, 2));
     }
     @Test
     public void itAlliensBulletNotCollapsedWithSpaceshipsBullet() {
-        Bullet bullet = new Bullet(3,2);
-        assertEquals(false, bullet.isCollapsedBullet(1, 3, 1, 2));
-    } 
+        Bullet Bullet = new Bullet("Spaceship",3,2);
+        assertEquals(false, Bullet.isCollapsedBullet(1, 3, 1, 2));
+    }
 }
