e1a4e9d4403a02c76f14aa0140daaa3f18874c35
SaulCM <saulcaspamiranda@gmail.com>
2021-02-06 15:18:39 -0400

    Class Alien Group add aliens

---
+++ src/test/java/org/fundacionjala/at13/spaceinvaders/AlienGroupTest.java
@@ -0,0 +1,15 @@
+package org.fundacionjala.at13.spaceinvaders;   
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AlienGroupTest {
+    @Test
+    public void shouldFillArrayWithAliens() {
+        int numberOfRowsForAliens = 3;
+        int numberOfColumnsForAliens = 5;
+        // 3 * 5 = 15 aliens
+        int numberOfAliensExpected = 15;
+        AlienGroup alienGroup = new AlienGroup(3, 5);
+        assertEquals(numberOfAliensExpected, alienGroup.getAliens().size());
+    }
+}
