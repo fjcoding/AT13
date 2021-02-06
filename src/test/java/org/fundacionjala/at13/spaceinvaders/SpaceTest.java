0afebfa3439b9f851e894c4624adf431088b972e
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-02-06 18:42:18 -0400

    add tests in spaceTest for Alines and Bullet

--- src/test/java/org/fundacionjala/at13/spaceinvaders/SpaceTest.java
+++ src/test/java/org/fundacionjala/at13/spaceinvaders/SpaceTest.java
@@ -50,4 +50,68 @@ public class SpaceTest {
         };
         assertArrayEquals(expected, spacePrint);
     }
+
+    @Test
+    public void itShouldShowSpaceWithAliens() {
+        int height = 10;
+        int width = 9;
+        int alien1InitPosX = 1;
+        int alien1InitPosY = 8;
+        int alien2InitPosX = 2;
+        int alien2InitPosY = 8;
+        int alien3InitPosX = 3;
+        int alien3InitPosY = 8;
+
+        Space space = new Space(height, width);
+        Alien[] aliens = {
+                new Alien(alien1InitPosX,alien1InitPosY),
+                new Alien(alien2InitPosX,alien2InitPosY),
+                new Alien(alien3InitPosX,alien3InitPosY),
+        };
+        space.setAlien(aliens);
+
+        String[][] spacePrint = space.show();
+        String[][] expected = {
+                /* y = 9 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 8 */{EMPTY, ALIEN, ALIEN, ALIEN, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 7 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 6 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 5 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 4 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 3 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 2 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 1 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 0 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /*          x = 0  x = 1  x = 2  x = 3  x = 4  x = 5  x = 6  x = 7  x = 8 */
+        };
+        assertArrayEquals(expected, spacePrint);
+    }
+
+    @Test
+    public void itShouldShowSpaceWithBullet() {
+        int height = 10;
+        int width = 9;
+        int bulletInitPosX = 4;
+        int bulletInitPosY = 1;
+
+        Space space = new Space(height, width);
+        Bullet bullet = new Bullet("Spaceship",bulletInitPosX, bulletInitPosY);
+        space.setBullet(bullet);
+
+        String[][] spacePrint = space.show();
+        String[][] expected = {
+                /* y = 9 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 8 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 7 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 6 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 5 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 4 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 3 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 2 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 1 */{EMPTY, EMPTY, EMPTY, EMPTY, BULLET, EMPTY, EMPTY, EMPTY, EMPTY},
+                /* y = 0 */{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                /*          x = 0  x = 1  x = 2  x = 3  x = 4  x = 5  x = 6  x = 7  x = 8 */
+        };
+        assertArrayEquals(expected, spacePrint);
+    }
 }
