c657631170462c6cd63ce43f3ab15d9b7cee6385
Paola-Xaq <paola.xaq@gmail.com>
2021-02-07 17:33:38 -0400

    Update, fix and add methods and test to Bullet.java and bulletTest
    Fix and add methods in Bullet java
    	Improving methods to use for Alien and Spaceship class
    	Improve method bullet moving for Alien and Spaceship
    	Fix previos errors with limits in moving Bullet
    
    Add test for each of the methods added, coverage of code at 100%, branches 80%

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Bullet.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Bullet.java
@@ -3,13 +3,19 @@ public class Bullet {
     private int positionInitialX;
     private int positionInitialY;
     private boolean bulletExist;
-    private String type;
+    private final int lowerLimit = 0;
+    private final int upperLimit = 50;
 
-    public Bullet(final String shooterType, final int x, final int y) {
-        positionInitialX = x;
-        positionInitialY = y;
+    public Bullet(final Spaceship spaceship) {
+        positionInitialX = spaceship.getPosX();
+        positionInitialY = spaceship.getPosY() + 1;
         bulletExist = false;
-        this.type = shooterType;
+    }
+
+    public Bullet(final Alien alien) {
+        positionInitialX = alien.getXPos();
+        positionInitialY = alien.getYPos() - 1;
+        bulletExist = false;
     }
 
     /**
@@ -41,33 +47,86 @@ public class Bullet {
 
     /**
      * Description: The function displays a bullet shoot.
+     * from the Spaceship.
      *
-     * @return a String that shoots a bullet.
      */
-    public String itShooting() {
+    public void isShootingToAlien(final Spaceship spaceship) {
         if (!bulletExist) {
-            bulletExist = true;
-            return "The " + this.type + " has fired from "
-            + this.positionInitialX
-            + " " + this.positionInitialY;
-        } else {
-            return "The " + this.type + " cannot fired.";
+            movingBulletUp();
+        }
+
+    }
+
+    /**
+     * Description: The function displays a bullet shoot.
+     * from the Alien.
+     *
+     */
+    public void isShootingToSpaceship(final Alien alien) {
+        if (!bulletExist) {
+            movingBulletDown();
         }
     }
 
     /**
      * Description: The function verifies if the bullet collapses.
      *
-     * @return a boolean that verifies if the bullet collapses.
+     * @return a boolean that verifies if the bullet  from Spaceship
+     * collapses with alien.
      */
-    public boolean isCollapsedBullet(final int positionX1, final int positionY1,
-    final int positionX2, final int positionY2) {
-        if (positionX1 == positionX2 && positionY1 == positionY2) {
-            System.out.println("The bullet has collapsed");
+    public boolean isCollapsedBulletWithAlien(final Alien alien) {
+        if (this.getPositionX() == alien.getXPos() && this.positionInitialY == alien.getYPos()) {
+            System.out.println("The bullet has kill Alien");
             bulletExist = false;
             return true;
         }
         return false;
+    }
+
+    /**
+     * Description: The function verifies if the bullet collapses.
+     *
+     * @return a boolean that verifies if the bullet  from Alien
+     * collapses with Spaceship.
+     */
+    public boolean isCollapsedBulletWithSpacceship(final Spaceship spaceship) {
+        if (this.getPositionX() == spaceship.getPosX() && this.positionInitialY == spaceship.getPosY()) {
+            System.out.println("The bullet has impact Spaceship");
+            bulletExist = false;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Description: The function move the bullet shoot Up.
+     *
+     */
+    public void movingBulletUp() {
+        if (positionInitialY < upperLimit) {
+            positionInitialY += 1;
+        } else if (positionInitialY == upperLimit) {
+            bulletExist = false;
+            System.out.print("Spaceship has failed.");
+        } else {
+            System.out.print("Error: Bullet is out of range.");
+        }
+
+    }
+
+    /**
+     * Description: The function move the bullet shoot Down.
+     *
+     */
+    public void movingBulletDown() {
+        if (positionInitialY > lowerLimit) {
+            positionInitialY -= 1;
+        } else if (positionInitialY < lowerLimit) {
+            bulletExist = false;
+            System.out.print("Alien has failed.");
+        } else {
+            System.out.print("Error: Bullet is out of range.");
+        }
     }
 
 }
