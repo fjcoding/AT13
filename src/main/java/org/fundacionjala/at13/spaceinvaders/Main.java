5dc850a6739f7b13f4d7aa693bef7ceb9142bd8f
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-02-07 18:44:02 -0400

    fixing conflicts in the main class

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Main.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Main.java
@@ -18,56 +18,19 @@ public final class Main {
         Space space = new Space(Space.DEFAULT_HEIGHT, Space.DEFAULT_WIDTH);
         Spaceship spaceship = new Spaceship(Spaceship.DEFAULT_POSX, Spaceship.DEFAULT_POSY);
         space.setSpaceship(spaceship);
-<<<<<<< HEAD
-        Alien[] aliens = {
-                new Alien(Alien.DEFAULT_POSX, Alien.DEFAULT_POSY),
-                new Alien(Alien.DEFAULT_POSX - 1, Alien.DEFAULT_POSY),
-                new Alien(Alien.DEFAULT_POSX - 2, Alien.DEFAULT_POSY)
-        };
-        Bullet bulletSpaceship = new Bullet(spaceship);
-        space.setBullet(bulletSpaceship);
-        space.setAlien(aliens);
 
-        print(space.show());
-        spaceship.moveToRight();
-        bulletSpaceship.isShootingToAlien(spaceship);
-        aliens[0].moveRight();
-        aliens[1].moveRight();
-        aliens[2].moveRight();
-        Thread.sleep(WAIT_PERIOD);
-        print(space.show());
-        spaceship.moveToRight();
-        aliens[0].moveRight();
-        aliens[1].moveRight();
-        aliens[2].moveRight();
-        aliens[0].moveDown();
-        bulletSpaceship.isShootingToAlien(spaceship);
-        Thread.sleep(WAIT_PERIOD);
-        print(space.show());
-=======
         AlienGroup alienGroup = new AlienGroup(ROWS, COLUMNS);
-        Bullet bullet = new Bullet("SpaceShip", spaceship.getPosX(), spaceship.getPosY() + 1);
-        space.setBullet(bullet);
+        //Bullet bullet = new Bullet("SpaceShip", spaceship.getPosX(), spaceship.getPosY() + 1);
+        //space.setBullet(bullet);
         space.setAlien(alienGroup.getAliens());
 
-//        print(space.show());
-//        spaceship.moveToRight();
-//        bullet.itShooting();
-//        alienGroup.aliensMoveRight();
-//        Thread.sleep(WAIT_PERIOD);
-//        print(space.show());
-//        spaceship.moveToRight();
-//        Thread.sleep(WAIT_PERIOD);
-//        print(space.show());
-
-        while (true) {
+       while (true) {
             print(space.show());
             spaceship.moveToRight();
-            bullet.itShooting();
+            //bullet.itShooting();
             alienGroup.moveAliens();
             Thread.sleep(WAIT_PERIOD);
         }
->>>>>>> 3230ae1 (change in the Space class to receive an Arraylist<Alien>)
     }
 
     private static void print(final String[][] matrix) {
