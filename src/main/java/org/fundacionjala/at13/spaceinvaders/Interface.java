e2588e0e80b834cf59517f5acab1d99140559d39
cristianfchq <cristian.fchq@gmail.com>
2021-02-09 15:51:24 -0400

    include frame and show windows

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Interface.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Interface.java
@@ -4,6 +4,11 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Interface extends JFrame {
+    private final JFrame frame = new JFrame();
+    private final JPanel panel = new JPanel();
+    private static final int WIDTH_WINDOWS = 500;
+    private static final int HEIGH_WINDOWS = 500;
+
     public Interface() {
         init();
     }
@@ -12,5 +17,22 @@ public class Interface extends JFrame {
      * Method initialize the graphical interface.
      */
     public void init() {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                frame.getContentPane().add(panel);
+                panel.setFocusable(true);
+                panel.requestFocusInWindow();
+                panel.setLayout(null);
+                panel.setBackground(Color.BLACK);
+                frame.setSize(new Dimension(WIDTH_WINDOWS, HEIGH_WINDOWS));
+                frame.setVisible(true);
+                frame.setTitle("Jala AT13 - Space Invanders");
+                frame.setLocationRelativeTo(null);
+                frame.setResizable(false);
+                frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            }
+        };
+        SwingUtilities.invokeLater(r);
     }
 }
