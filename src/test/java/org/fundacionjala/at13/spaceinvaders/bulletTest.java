fc14add50558175d0bdbeff38444acdb0224cdff
Joel Rojas <jr.bestone17@gmail.com>
2021-02-08 22:26:08 -0400

    Add test methods to prove others constructor of bullet class

--- src/test/java/org/fundacionjala/at13/spaceinvaders/bulletTest.java
+++ src/test/java/org/fundacionjala/at13/spaceinvaders/bulletTest.java
@@ -6,6 +6,36 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class bulletTest{
+
+
+    int lowerXLimit=0;
+    int upperXLimit=10;
+    int positionInitialX = 0;
+    int positionInitialY = 9;
+    int posXInitAlien = 5;
+    int posYInitAlien = 5;
+
+    @Test
+    public void itShouldGetBulletPositiionInXForSpaceship() {
+        Spaceship spaceship=new Spaceship(positionInitialX,positionInitialY,lowerXLimit,upperXLimit);
+        Bullet Bullet = new Bullet(spaceship);
+        assertEquals(0, Bullet.getPositionX());        
+    }
+
+    @Test
+    public void itShouldGetBulletPositionInYForSpaceship() {
+        Spaceship spaceship=new Spaceship(positionInitialX,positionInitialY,lowerXLimit,upperXLimit);
+        Bullet Bullet = new Bullet(spaceship);
+        assertEquals(10, Bullet.getPositionY());        
+    }
+
+/*     @Test
+    public void itShouldSayIfSpaceShipHasShootAnotherBullet() {
+        Spaceship spaceship=new Spaceship(positionInitialX,positionInitialY,lowerXLimit,upperXLimit);
+        Bullet bullet = new Bullet(spaceship);
+        bullet.isShootingToAlien();
+        assertFalse(bullet.getBulletExist());
+    } */
 /* 
     @Test
     public void itShouldGetBulletPositionInXForSpaceship() {
