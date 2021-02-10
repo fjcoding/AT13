0f65229d0ee2b7fb9a4e7fb69d3fe0a56a1c2095
Gustavo Huanca <gustavozha@gmail.com>
2021-02-10 15:30:50 -0400

    Add new method which calculete how many lives spaceshis has

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Spaceship.java
@@ -4,7 +4,8 @@ public class Spaceship extends SpaceElement {
 
     public static final int DEFAULT_POSX = 0;
     public static final int DEFAULT_POSY = 0;
-
+    private static final int FULL_LIFE = 3;
+    private int numbersLife = FULL_LIFE;
     public Spaceship(final int pX, final int pY, final int llimit, final int ulimit) {
         super(pX, pY, llimit, ulimit);
     }
@@ -14,5 +15,17 @@ public class Spaceship extends SpaceElement {
      */
     public Bullet shoot() {
         return new Bullet(this);
+    }
+
+    /**
+     * @return Ask if spacehip has more than 3 lives
+     */
+    public void isStillAlive() {
+        if (numbersLife > 0) {
+            this.numbersLife -= 1;
+        }
+        if (numbersLife == 0) {
+            System.out.print("Game Over");
+        }
     }
 }
