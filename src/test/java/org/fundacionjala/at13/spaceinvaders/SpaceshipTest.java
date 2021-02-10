450b2e60b7503f835d2107413936752be6d80afa
Gustavo Huanca <gustavozha@gmail.com>
2021-02-10 16:09:01 -0400

    Add unit test itShouldReturnNumbersLife and itShouldSayIfSpaceshipIsNotStillAlive for spaceShip

--- src/test/java/org/fundacionjala/at13/spaceinvaders/SpaceshipTest.java
+++ src/test/java/org/fundacionjala/at13/spaceinvaders/SpaceshipTest.java
@@ -6,6 +6,8 @@ import java.lang.annotation.Target;
 
 import org.junit.Test;
 
+import jdk.jfr.Timestamp;
+
 public class SpaceshipTest{
 
     /*@Test
@@ -20,6 +22,15 @@ public class SpaceshipTest{
         Bullet bullet=new Bullet(shape,positionInitialX,positionInitialY);
         assertTrue(bullet.equals(bulletShootBySpaceship));
     }*/
+    @Test
+    public void itShouldReturnNumbersLife() {
+        int lowerXLimit=0;
+        int upperXLimit=42;
+        int positionInitSpaceshipX = 4;
+        int positionInitSpaceshipY = 0;
+        Spaceship spaceship = new Spaceship(positionInitSpaceshipX, positionInitSpaceshipY,lowerXLimit,upperXLimit);
+        assertEquals(3,spaceship.getNumbersLife());
+    }
     @Test
     public void itShouldSayIfSpaceshipStillAlive() {
         int lowerXLimit=0;
@@ -38,5 +49,25 @@ public class SpaceshipTest{
         assertTrue(bulletAlien.isCollapsedBulletWithSpaceship(spaceship));
         assertEquals(2,spaceship.getNumbersLife());
     }
-
+    @Test
+    public void itShouldSayIfSpaceshipIsNotStillAlive() {
+        int lowerXLimit=0;
+        int upperXLimit=12;
+        int positionInitAlienX = 4;
+        int positionInitAlienY = 6;
+        int positionInitSpaceshipX = 4;
+        int positionInitSpaceshipY = 5;
+        Alien alien = new Alien(positionInitAlienX, positionInitAlienY,lowerXLimit,upperXLimit);
+        Spaceship spaceship = new Spaceship(positionInitSpaceshipX, positionInitSpaceshipY,lowerXLimit,upperXLimit);
+        Bullet bulletAlien = new Bullet(alien);
+        //First Impact
+        assertTrue(bulletAlien.isCollapsedBulletWithSpaceship(spaceship));
+        assertEquals(2,spaceship.getNumbersLife());
+        //Secont Impact
+        assertTrue(bulletAlien.isCollapsedBulletWithSpaceship(spaceship));
+        assertEquals(1,spaceship.getNumbersLife());
+        //Third Impact
+        assertTrue(bulletAlien.isCollapsedBulletWithSpaceship(spaceship));
+        assertEquals(0,spaceship.getNumbersLife());
+    }
 }
