d1dc2fc68e5cccca9bd27049e3b2ff77e99f97bf
SaulCM <saulcaspamiranda@gmail.com>
2021-02-11 16:33:34 -0400

    alien moves in grids

--- src/main/java/org/fundacionjala/at13/spaceinvaders/Gameboard.java
+++ src/main/java/org/fundacionjala/at13/spaceinvaders/Gameboard.java
@@ -19,25 +19,7 @@ public class Gameboard {
     private static final int SIZE_IMAGE = 20;
     private static final int SCALE_WIDTH = Constants.SPACE_SIZE_WIDTH * 5;
     private static final int SCALE_HEIGHT = Constants.SPACE_SIZE_HEIGHT * 15;
-    private final Spaceship spaceship = new Spaceship((SCALE_WIDTH - SIZE_IMAGE) / 2, SCALE_HEIGHT - SIZE_IMAGE, 0, SCALE_WIDTH - 23);
-
-    /**
-     * Create labels for the panel grids and add them.
-     */
-    public void fillGridsWithLabels() {
-        space = new JLabel[Constants.SPACE_SIZE_HEIGHT][Constants.SPACE_SIZE_WIDTH];
-        //setLayout(new GridLayout(Constants.SPACE_SIZE_HEIGHT, Constants.SPACE_SIZE_WIDTH, 0, 0));
-        for (int row = 0; row < Constants.SPACE_SIZE_HEIGHT; row++) {
-            for (int col = 0; col < Constants.SPACE_SIZE_WIDTH; col++) {
-                JLabel label = new JLabel();
-                label.setOpaque(true);
-                label.setBackground(Color.black);
-                label.setVisible(true);
-                space[row][col] = label;
-                panel.add(label);
-            }
-        }
-    }
+    private final Spaceship spaceship = new Spaceship(Constants.SPACESHIP_INITIAL_X_POS, Constants.SPACESHIP_INITIAL_Y_POS, Constants.INITIAL_RANGE, Constants.SPACE_SIZE_WIDTH);
 
     public Gameboard() {
         init();
@@ -63,12 +45,30 @@ public class Gameboard {
                 panel.setLayout(new GridLayout(Constants.SPACE_SIZE_HEIGHT, Constants.SPACE_SIZE_WIDTH, 0, 0));
                 fillGridsWithLabels();
                 //spaceShipImage(POS_X, POS_Y);
+                updateSpace();
             }
         };
         SwingUtilities.invokeLater(r);
         eventKey();
+        updateSpace();
     }
-
+    /**
+     * Create labels for the panel grids and add them.
+     */
+    public void fillGridsWithLabels() {
+        space = new JLabel[Constants.SPACE_SIZE_HEIGHT][Constants.SPACE_SIZE_WIDTH];
+        //setLayout(new GridLayout(Constants.SPACE_SIZE_HEIGHT, Constants.SPACE_SIZE_WIDTH, 0, 0));
+        for (int row = 0; row < Constants.SPACE_SIZE_HEIGHT; row++) {
+            for (int col = 0; col < Constants.SPACE_SIZE_WIDTH; col++) {
+                JLabel label = new JLabel();
+                label.setOpaque(true);
+                label.setBackground(Color.black);
+                label.setVisible(true);
+                space[row][col] = label;
+                panel.add(label);
+            }
+        }
+    }
     /**
      * Method initialize the image.
      */
@@ -76,7 +76,23 @@ public class Gameboard {
         //ship.setBounds(posX, posY, SIZE_IMAGE, SIZE_IMAGE);
         //panel.add(ship);
     }
-
+    public void updateSpace() {
+        for (int row = 0; row < Constants.SPACE_SIZE_HEIGHT; row++) {
+            for (int col = 0; col < Constants.SPACE_SIZE_WIDTH; col++) {
+                if (row == spaceship.getPosY() && col == spaceship.getPosX()) {
+                    ImageIcon iconLogo = new ImageIcon("src/resources/spaceship.png");
+                    space[row][col].setIcon(iconLogo);
+                }
+            }
+        }
+    }
+    public void clearSpace() {
+        for (int row = 0; row < Constants.SPACE_SIZE_HEIGHT; row++) {
+            for (int col = 0; col < Constants.SPACE_SIZE_WIDTH; col++) {
+                space[row][col].setIcon(null);
+            }
+        }
+    }
     /**
      * Method for the key.
      */
@@ -89,12 +105,15 @@ public class Gameboard {
             @Override
             public void keyPressed(final KeyEvent e) {
                 if (e.getKeyCode() == GO_LEFT) {
+                    clearSpace();
                     spaceship.moveLeft();
-
+                    updateSpace();
                     //ship.setBounds(spaceship.getPosX(), POS_Y, SIZE_IMAGE, SIZE_IMAGE);
                 }
                 if (e.getKeyCode() == GO_RIGHT) {
+                    clearSpace();
                     spaceship.moveRight();
+                    updateSpace();
                     //ship.setBounds(spaceship.getPosX(), POS_Y, SIZE_IMAGE, SIZE_IMAGE);
                 }
             }
