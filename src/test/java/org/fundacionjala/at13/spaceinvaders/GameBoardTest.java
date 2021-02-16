3c5cc11e262e7f681e75a63e462a5fff4e88e00d
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-02-16 19:56:36 -0400

    fix conflict en gameboarttest

--- src/test/java/org/fundacionjala/at13/spaceinvaders/GameBoardTest.java
+++ src/test/java/org/fundacionjala/at13/spaceinvaders/GameBoardTest.java
@@ -1,8 +1,17 @@
 package org.fundacionjala.at13.spaceinvaders;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.Test;
-public class GameBoardTest {
+import org.sikuli.script.Screen;
 
-    
+public class GameBoardTest {
+    @Test
+    public void testPruebaSikuli() throws InterruptedException {
+        Screen s = new Screen();
+        GameBoard gameBoard = new GameBoard();
+        gameBoard.init();
+        Thread.sleep(500);
+        Assert.assertNotNull(s.exists(System.getProperty("user.dir")+"\\resources\\space.png"));
+    }
+
 }
